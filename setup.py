0b30e811f0c3057c6e33332e32a7e3388fe461fa
Michael Weiß <michael.weiss@jku.at>
2025-02-06 14:41:57 +0100

    Setup.py created

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+   name='BubbleFinder',
+   version='1.0',
+   description='Easy and robust deep learning bubble segmentation',
+   author='t.a.m.homan@tue.nl',
+   author_email='t.a.m.homan@tue.nl',
+   url='https://pubs.acs.org/doi/10.1021/acs.iecr.3c04059'
+   install_requires=[
+        'matplotlib', 
+        'numpy', 
+        'pandas', 
+        'Pillow'
+        'pycocotools',
+        'scipy',
+        'torch',
+        'torchvision',
+        'tqdm',
+        'trackpy',
+        'opencv-python']
+)
